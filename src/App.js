5839591f9c515f9c789e3bc854d5eab5fccc5675
JessicaMP <jessica95.mp@gmail.com>
2018-05-15 15:54:33 -0500

    Corrigiendo estructura

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Header from './components/Header';
+import Nav from './components/Nav';
+import Title from './components/Title';
+import Main from './components/Main';
+import Aside from './components/Aside';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <Header/>
+      <Nav/>
+      <Title/>
+      <Main/>
+      <Aside/>
+      </div>
+    );
+  }
+}
+
+export default App;
